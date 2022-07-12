4847f1d0c51878267b1eb104fd3fd7a6f7764039
= <=>
2022-07-13 00:47:26 +0530

    Spring Built In scheduler demo

---
+++ src/test/java/duega/SchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package duega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
